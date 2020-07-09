37dbcac70db977e5e3beddab2014b2d6fe20fc8b
Barry Sanders <barry.sanders@endpointiq.com.au>
2020-07-09 10:23:49 +1000

    Create web ui project and add entities

---
+++ CaWorkshop/CaWorkshop.WebUI/Models/TodoItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CaWorkshop.WebUI.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+
+        public int ListId { get; set; }
+
+        [Required]
+        [StringLength(280)]
+        public string Title { get; set; }
+
+        [StringLength(4000)]
+        public string Note { get; set; }
+
+        public bool Done { get; set; }
+
+        public DateTime? Reminder { get; set; }
+
+        public PriorityLevel Priority { get; set; }
+
+        public TodoList List { get; set; }
+    }
+}
