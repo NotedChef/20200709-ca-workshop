f0e655254023af67bcd74b03a160d51e5e43d79e
Barry Sanders <barry.sanders@endpointiq.com.au>
2020-07-09 14:33:40 +1000

    Move files into projects. Move dependency injection into projects and slim down startup

--- CaWorkshop/CaWorkshop.Domain/Entities/PriorityLevel.cs
+++ CaWorkshop/CaWorkshop.Domain/Entities/PriorityLevel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CaWorkshop.WebUI.Models
+namespace CaWorkshop.Domain.Entities
 {
     public enum PriorityLevel
     {
