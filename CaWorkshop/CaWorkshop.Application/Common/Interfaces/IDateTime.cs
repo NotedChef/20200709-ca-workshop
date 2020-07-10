5b233e6ae2d17e917e6dde8748a15012edb601a1
Barry Sanders <barry.sanders@endpointiq.com.au>
2020-07-10 15:55:25 +1000

    Added config.nswag from nswagstudio. and generated specification and  typescript client

---
+++ CaWorkshop/CaWorkshop.Application/Common/Interfaces/IDateTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaWorkshop.Application.Common.Interfaces
+{
+    public interface IDateTime
+    {
+     public DateTime UtcNow { get; set; }   
+    }
+}
